11834ffe69bee6199e3b1a1d5a16da8ce02933b9
BrianX340 <brianx340@gmail.com>
2021-06-25 16:31:47 -0300

    Hasta aca se soluciono creacion de cuentas de usuario

--- facebloog/static/js/index.js
+++ facebloog/static/js/index.js
@@ -13,22 +13,20 @@ window.onload = () => {
                 document.getElementById('lastName').focus()
             } else if (elementId == 'lastName'){
                 document.getElementById('username').focus()
+            } else if (elementId == 'emailRegister'){
+                document.getElementById('username').focus()
             } else if (elementId == 'username'){
                 document.getElementById('passwordregister').focus()
-            } else if (elementId == 'passwordregister'){
+            } else if(elementId == 'passwordregister'){
                 document.getElementById('passwordconfirm').focus()
             } else if (elementId == 'passwordconfirm'){
                 name = document.getElementById('name').value
                 lastName = document.getElementById('lastName').value
+                email = document.getElementById('emailRegister').value
                 username = document.getElementById('username').value
-                passwordregister = document.getElementById('passwordregister').value
+                password = document.getElementById('passwordregister').value
                 if ( name && lastName && username && passwordregister){
-                    registrarUsuario({
-                        name,
-                        lastName,
-                        username,
-                        passwordregister
-                    })
+                    registrarUsuario({ email, name, lastName, username, password })
                 }
             }
             event.preventDefault()
@@ -41,15 +39,12 @@ window.onload = () => {
             name = document.getElementById('name').value
             lastName = document.getElementById('lastName').value
             username = document.getElementById('username').value
-            passwordregister = document.getElementById('passwordregister').value
-            if ( name && lastName && username && passwordregister){
-                registrarUsuario({
-                    name,
-                    lastName,
-                    username,
-                    passwordregister
-                })
+            password = document.getElementById('passwordregister').value
+            if ( name && lastName && username && password){
+                registrarUsuario({ email:'brianx340', name, lastName, username, password })
             }
+        } else if (elementId=='volver'){
+            quitarPopup()
         }
     })
 
@@ -59,48 +54,50 @@ window.onload = () => {
 function clearForm() {
     document.getElementById('name').value = ''
     document.getElementById('lastName').value = ''
-    document.getElementById('emailregister').value = ''
+    document.getElementById('emailRegister').value = ''
+    document.getElementById('username').value = ''
     document.getElementById('passwordregister').value = ''
     document.getElementById('passwordconfirm').value = ''
 }
 
 
 function registrarUsuario(user){
-
-    fetch(`http://localhost:3000/api/Auth/register`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(user),
-        cache: "no-cache",
-        headers: new Headers({
-            "content-type": "application/json"
-        })
-    }).then((response) => {if (response.status !== 200) {
-        console.log(`Looks like there was a problem. Status code: ${response.status}`);
-        return;
-    }
-    response.json().then(function (data) {
-        console.log(data)
-    });
-    }).catch(function (error) {
-        console.log("Fetch error: " + error);
-    });
-
-
-    console.log('registrando')
-    clearForm()
+    fetch('https://serverless-660nscdk6-brianx340.vercel.app/api/Auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        clearForm()
+        if (data.status == 'ok'){
+            crearPopup('Bienvenido a Facebloog','Su cuenta se ha creado satisfactoriamente.')
+        } else if (data.status == 'error'){
+            crearPopup('Ops! ha ocurrido un error',data.content)
+        }
+    })
 }
 
+function crearPopup(titulo,mensaje){
+    document.getElementById('contMessage').innerHTML = "<div id='popup-wrapper' class='popup-wrapper'><div class='popup'><div class='containerClose'><div class='popup-close'>x</div></div><div class='popup-content'><h3>TITULO</h3><h2></h2><p>MENSAJE</p><div class='backContainer'><a id='volver' target='_blank'>Volver</a></div></div></div></div>".replace('TITULO', titulo).replace('MENSAJE', mensaje)
+    document.getElementById('popup-wrapper').style.display = 'flex'
+}
+
+function quitarPopup(){
+    document.getElementById('contMessage').innerHTML = ""
+}
 
 /*
-<div id='popup-wrapper' class="popup-wrapper">
-    <div class="popup">
-        <div class="popup-close">x</div>
-        <div class="popup-content">
+<div id='popup-wrapper' class='popup-wrapper'>
+    <div class='popup'>
+        <div class='popup-close'>x</div>
+        <div class='popup-content'>
             <h3>Bienvenido a Facebloog</h3>
             <h2></h2>
             <p>¡Gracias por registrarse!</p>
-            <a id="volver" href="#" target="_blank">Volver</a>
+            <a id='volver' href='#' target='_blank'>Volver</a>
         </div>
     </div>
 </div>
